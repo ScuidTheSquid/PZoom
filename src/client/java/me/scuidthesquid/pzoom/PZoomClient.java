55a3d5f9a74ca4ba1dd3ccce984b15e036dff5c2
Paul Schnisa <paul.schnisa@proton.me>
2023-09-03 21:02:07 +0200

    changed readme

--- src/client/java/me/scuidthesquid/pzoom/PZoomClient.java
+++ src/client/java/me/scuidthesquid/pzoom/PZoomClient.java
@@ -4,8 +4,10 @@ import me.scuidthesquid.pzoom.event.KeyInputHandler;
 import net.fabricmc.api.ClientModInitializer;
 
 public class PZoomClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
 		KeyInputHandler.register();
+
 	}
 }
